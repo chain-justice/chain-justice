d8313158bb689c5b31a785cb3df885303da7bb46
shoheitakaichi <shohei.takaichi@gmail.com>
2022-10-31 19:29:02 +0900

    BelongCountry implement #8

--- x/justice/keeper/msg_server_msg_belong_contry.go
+++ x/justice/keeper/msg_server_msg_belong_contry.go
@@ -5,13 +5,35 @@ import (
 
 	"github.com/chain-justice/chain-justice/x/justice/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) MsgBelongContry(goCtx context.Context, msg *types.MsgMsgBelongContry) (*types.MsgMsgBelongContryResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	_, isFound := k.GetCountry(
+		ctx,
+		msg.CountryAddress,
+	)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "No Such Country")
+	}
 
+	_, isFound = k.GetBelonging(
+		ctx,
+		msg.Creator,
+	)
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "already belonging")
+	}
+
+	var belonging = types.Belonging{
+		Creator: msg.Creator,
+		Index:   msg.Creator,
+		Address: msg.Creator,
+		Country: msg.CountryAddress,
+	}
+
+	k.SetBelonging(ctx, belonging)
 	return &types.MsgMsgBelongContryResponse{}, nil
 }
