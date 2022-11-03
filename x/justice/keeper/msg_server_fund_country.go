2f3bdabbf0b53c34c79a4d4ae11188899df483ef
shoheitakaichi <shohei.takaichi@gmail.com>
2022-11-03 17:53:15 +0900

    implement message

--- x/justice/keeper/msg_server_fund_country.go
+++ x/justice/keeper/msg_server_fund_country.go
@@ -5,13 +5,48 @@ import (
 
 	"github.com/chain-justice/chain-justice/x/justice/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) FundCountry(goCtx context.Context, msg *types.MsgFundCountry) (*types.MsgFundCountryResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
+	_, isFound := k.GetCountry(
+		ctx,
+		msg.Creator,
+	)
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "country is already funded")
+	}
+	var country = types.Country{
+		Index:   msg.Creator,
+		Address: msg.Creator,
+		Food:    "0",
+	}
+	_, isFound = k.GetBelonging(
+		ctx,
+		msg.Creator,
+	)
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "you already belong to some country")
+	}
+
+	var belonging = types.Belonging{
+		Index:   msg.Creator,
+		Address: msg.Creator,
+		Country: msg.Creator,
+	}
+
+	k.SetBelonging(
+		ctx,
+		belonging,
+	)
+
+	k.SetCountry(
+		ctx,
+		country,
+	)
 
 	return &types.MsgFundCountryResponse{}, nil
 }
--- x/justice/keeper/msg_server_invasion_start.go
+++ x/justice/keeper/msg_server_invasion_start.go
@@ -2,16 +2,38 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/chain-justice/chain-justice/x/justice/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) InvasionStart(goCtx context.Context, msg *types.MsgInvasionStart) (*types.MsgInvasionStartResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	
+	_, isFound := k.GetBelonging(
+		ctx,
+		msg.Creator,
+	)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "You have to belong to a contry")
+	}
 
-	// TODO: Handling the message
-	_ = ctx
+	_, isFound = k.GetCountry(
+		ctx,
+		msg.CountryAddress,
+	)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "No Such Country")
+	}
 
+	var invasion = types.Invasion{
+		Index:   msg.Creator,
+		FromAddress: msg.Creator,
+		ToAddress: msg.CountryAddress,
+		RequireBlockHeigt: strconv.FormatInt(ctx.BlockHeight(),10),
+	}
+	k.SetInvasion(ctx, invasion)
 	return &types.MsgInvasionStartResponse{}, nil
 }
