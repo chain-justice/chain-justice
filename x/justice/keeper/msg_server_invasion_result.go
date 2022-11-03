cde7cf045233dc59fd1bdd9f91c2659855063b08
shoheitakaichi <shohei.takaichi@gmail.com>
2022-11-03 20:16:54 +0900

    update logic

--- x/justice/keeper/msg_server_invasion_result.go
+++ x/justice/keeper/msg_server_invasion_result.go
@@ -2,16 +2,57 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/chain-justice/chain-justice/x/justice/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) InvasionResult(goCtx context.Context, msg *types.MsgInvasionResult) (*types.MsgInvasionResultResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	invasion, isFound := k.GetInvasion(
+		ctx,
+		msg.Creator,
+	)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "You didn't start invasion")
+	}
+
+	requireBlockHeigt, err := strconv.ParseInt(invasion.RequireBlockHeigt, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if ctx.BlockHeight() < requireBlockHeigt {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Not enough BlockHeigt. Please retry later")
+	}
+
+	_, isFoundBelonging := k.GetBelonging(
+		ctx,
+		msg.Creator,
+	)
+	if !isFoundBelonging {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "You don't belong to any country")
+	}
+
+	_, isFoundFromCountry := k.GetCountry(
+		ctx,
+		invasion.FromAddress,
+	)
+	if !isFoundFromCountry {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "No such country")
+	}
+
+	_, isFoundDestinyCountry := k.GetCountry(
+		ctx,
+		invasion.ToAddress,
+	)
+	if !isFoundDestinyCountry {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "No such country")
+	}
+
+	// to do change contry status
 
 	return &types.MsgInvasionResultResponse{}, nil
 }
--- x/justice/keeper/msg_server_prepare_result.go
+++ x/justice/keeper/msg_server_prepare_result.go
@@ -2,16 +2,49 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/chain-justice/chain-justice/x/justice/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) PrepareResult(goCtx context.Context, msg *types.MsgPrepareResult) (*types.MsgPrepareResultResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	prepare, isFound := k.GetPrepare(
+		ctx,
+		msg.Creator,
+	)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "You didn't start preparing")
+	}
+
+	requireBlockHeigt, err := strconv.ParseInt(prepare.RequireBlockHeigt, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if ctx.BlockHeight() < requireBlockHeigt {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Not enough BlockHeigt. Please retry later")
+	}
+
+	belonging, isFoundBelonging := k.GetBelonging(
+		ctx,
+		msg.Creator,
+	)
+	if !isFoundBelonging {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "You don't belong to any country")
+	}
+
+	_, isFoundCountry := k.GetCountry(
+		ctx,
+		belonging.Country,
+	)
+	if !isFoundCountry {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "No such country")
+	}
+
+	// to do change contry status
 
 	return &types.MsgPrepareResultResponse{}, nil
 }
