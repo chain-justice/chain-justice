62aa78c2659e8afc4cc230d66812bd8d3965536a
shuichi.fushimi <takattataka@gmail.com>
2022-11-06 02:03:49 +0900

    クライアントからFaucetを利用可能にする

---
+++ vue/src/components/faucet/SpFaucet/SpFaucet.vue
@@ -0,0 +1,38 @@
+<template>
+  <Suspense>
+    <template #default>
+      <SpRequestFaucet />
+    </template>
+    <template #fallback>
+      <div class="tx-list">
+        <div class="title"></div>
+      </div>
+    </template>
+  </Suspense>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useStore } from '@starport/vue/node_modules/vuex'
+
+import { useAddress } from '@starport/vue/src/composables'
+import SpRequestFaucet from '../SpRequestFaucet'
+
+export default defineComponent({
+  // TODO　このコンポーネントは SpTokenTransferList と被っているので一般化する
+  name: 'SpTokenTransferList',
+
+  components: { SpRequestFaucet },
+
+  setup() {
+    let $s = useStore()
+
+    // composables
+    let { address } = useAddress({ $s })
+
+    return { address }
+  }
+})
+</script>
+
+<style src="./SpFaucet.scss" lang="scss" scoped></style>
---
+++ vue/src/components/faucet/index.ts
@@ -0,0 +1,2 @@
+export { default as SpRequestFaucet } from './SpRequestFaucet'
+export { default as SpFaucet } from './SpFaucet' 
